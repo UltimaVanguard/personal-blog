02b321cd963beae7d4758b0ec3168f63f012d4c6
UltimaVanguard <ultimavanguard@gmail.com>
2024-04-03 08:42:00 -0500

    added comments

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,24 @@
+// constant variables
 const themeToggle = document.querySelector('#theme-switcher');
 const themeText = document.querySelector('.theme');
 const header = document.querySelector('header');
 const main = document.querySelector('main');
 const footer = document.querySelector('footer');
 
+// defining theme
 let theme = '';
 
+// function to pull in local storage
 const init = function() {
+    // gets blog array and theme from local storage
     let blogArray = JSON.parse(localStorage.getItem("blog"));
     theme = localStorage.getItem("theme");
 
+    // if blog array has content adds the content to the page
     if (blogArray !== null) {
+        // loops through the blog array to grab everything
         for (i = 0; i < blogArray.length; i++) {
+            // creates the elements that need to be added
             const h3 = document.createElement('h3');
             const pContent = document.createElement('p');
             const pName = document.createElement('p');
@@ -19,12 +26,14 @@ const init = function() {
             const topPBorder = document.createElement('p');
             const bottomPBorder = document.createElement('p');
 
+            // fills in the data from the array
             h3.textContent = blogArray[i].title;
             pContent.textContent = blogArray[i].content;
             pContent.classList.add("content");
             pName.textContent = "Author: " + blogArray[i].name;
             pName.classList.add("author")
 
+            // appends the elements to the main section
             document.body.children[1].children[0].appendChild(blogSection);
             document.body.children[1].children[0].children[i].appendChild(topPBorder);
             topPBorder.setAttribute('class', 'topPBorder');
@@ -36,6 +45,7 @@ const init = function() {
         }
     }
 
+    // changes theme to what's in local storage
     if (theme === 'dark') {
         document.body.setAttribute('class', 'dark-mode');
         header.setAttribute('class', "dark-mode");
@@ -47,6 +57,7 @@ const init = function() {
     }
 }
 
+// changes theme on theme toggle click
 themeToggle.addEventListener('click', function(event) {
     if (theme === 'dark') {
         document.body.setAttribute('class', 'light-mode');
@@ -64,7 +75,9 @@ themeToggle.addEventListener('click', function(event) {
         theme = "dark";
     }
 
+    // loads theme to local storage
     localStorage.setItem("theme", theme);
 });
 
+// runs initial function to set theme and load local storage
 init();
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,4 @@
+// const variables
 const formEl = document.querySelector('#blog-form');
 const nameInput = document.querySelector('#full-name');
 const titleInput = document.querySelector('#title');
@@ -8,13 +9,16 @@ const themeText = document.querySelector('.theme');
 const header = document.querySelector('header');
 const footer = document.querySelector('footer');
 
+// defining array and theme
 let blogArray = [];
 let theme = '';
 
+// initial function to pull array and theme from local storage
 let init = function() {
     blogArray = JSON.parse(localStorage.getItem("blog"));
     theme = localStorage.getItem("theme");
 
+    // setting current theme on load
     if (theme === 'dark') {
         document.body.setAttribute('class', 'dark-mode');
         header.setAttribute('class', "dark-mode");
@@ -25,26 +29,32 @@ let init = function() {
     }
 }
 
+// Function to submit form when submit is clicked
 formEl.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // defining a blog entry object
     const blogEntry = {
         name: nameInput.value,
         title: titleInput.value,
         content: contentInput.value,
     }
 
+    // checking if all fields are filled in
     if(!nameInput.value || !titleInput.value || !contentInput.value) {
        alert('Please finish filling out the form.');
         return;
     }
 
+    // pushing entry to the array, updating local storage, and going to the blogs page
     blogArray.push(blogEntry);
     localStorage.setItem("blog", JSON.stringify(blogArray));
     location.href='./blog.html';
 })
 
+// function to change theme on toggle click
 themeToggle.addEventListener('click', function(event) {
+    // checks what the current theme is and changes classes as necessary
     if (theme === 'dark') {
         document.body.setAttribute('class', 'light-mode');
         header.setAttribute('class', "light-mode");
@@ -59,7 +69,9 @@ themeToggle.addEventListener('click', function(event) {
         theme = "dark";
     }
 
+    // Loads theme to local storage
     localStorage.setItem("theme", theme);
 });
 
+// runs initial function on page load
 init();
